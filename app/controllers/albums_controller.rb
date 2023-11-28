29fcb2b6150b7f3098c75159bc6e49524e724db4
Gustavo Franco <gusfrancoherrera@gmail.com>
2023-11-28 15:27:43 +0100

    the card-medium and card-big classes have been created. The container-cards-medium has also been created. Additionally the body of the HTML docs has been set to black

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -8,13 +8,22 @@ class AlbumsController < ApplicationController
     @album = "2WT1pbYjLJciAR26yMebkH?si=9HU9lX21R7ehUYAOQK9Arw"
     endpoint1 = RestClient.get(
       "https://api.spotify.com/v1/albums/#{@album}",
-      headers={ 'Authorization': "Bearer #{@access_token}" }
+      headers = { 'Authorization': "Bearer #{@access_token}" }
     )
     @data = JSON.parse(endpoint1)
     @album_name = @data['name']
-    @album_artist = @data['artists'[ 'name ']]
+    @album_artist = @data['artists'['name ']]
     @release_date = @data['release_date']
+    @parsed_release_date = DateTime.parse(@release_date) rescue nil
     @image = @data['url']
+    @popularity = @data['popularity']
+    @total_tracks = @data['total_tracks']
+    @genre = @data['genres']
+    total_duration_ms = @data['tracks']['items'].sum { |track| track['duration_ms'] }
+    total_seconds = total_duration_ms / 1000
+    minutes = total_seconds / 60
+    seconds = total_seconds % 60
+    @formatted_duration = "#{minutes}:#{seconds} min"
   end
 
   private
@@ -28,5 +37,4 @@ class AlbumsController < ApplicationController
 
     @access_token = JSON.parse(response.body)['access_token']
   end
-
 end
