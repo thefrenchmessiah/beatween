53fe1700f45d86cf25c99ccc17bf751af418146f
Federico Palou <federicopalou@gmail.com>
2023-11-29 17:38:20 +0100

    errors on artist buttons but otherwise working

--- app/javascript/controllers/switch_button_controller.js
+++ app/javascript/controllers/switch_button_controller.js
@@ -2,14 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="switch-button"
 export default class extends Controller {
+  static targets = ["info", "stats"]
   connect() {
     console.log("Switch Button Controller connected!");
   }
-
   switchToInfo() {
     console.log("Switching to info");
+    this.infoTarget.style.display = "block";
+    this.statsTarget.style.display = "none";
   }
+
   switchToStats() {
-    console.log("Switching to Stats");
+    console.log("Switching to stats");
+    this.infoTarget.style.display = "none";
+    this.statsTarget.style.display = "block";
   }
 }
