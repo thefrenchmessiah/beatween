f04b0153568786400a0e9a7d95613cd99a5118df
Gustavo Franco <gusfrancoherrera@gmail.com>
2023-11-29 15:34:56 +0100

    controller now generates album info dynamically

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -5,21 +5,21 @@ class AlbumsController < ApplicationController
   before_action :get_key
 
   def show
-    @album = "2WT1pbYjLJciAR26yMebkH?si=9HU9lX21R7ehUYAOQK9Arw"
-    endpoint1 = RestClient.get(
-      "https://api.spotify.com/v1/albums/#{@album}",
-      headers = { 'Authorization': "Bearer #{@access_token}" }
-    )
-    @data = JSON.parse(endpoint1)
-    @album_name = @data['name']
-    @album_artist = @data['artists'['name ']]
-    @release_date = @data['release_date']
+    @album_id = "4LH4d3cOWNNsVw41Gqt2kv"
+    # @data = JSON.parse(endpoint1)
+    album = RSpotify::Album.find(@album_id)
+    @album_name = album.name
+    # @album_name = @data['name']
+    # @album_artist = @data['artists'['name ']]
+    @album_artist = album.artists[0].name
+    # @release_date = @data['release_date']
+    @release_date = album.release_date
     @parsed_release_date = DateTime.parse(@release_date) rescue nil
-    @album_image = @data['images'][0]['url']
-    @popularity = @data['popularity']
-    @total_tracks = @data['total_tracks']
-    @genre = @data['genres']
-    total_duration_ms = @data['tracks']['items'].sum { |track| track['duration_ms'] }
+    @album_image = album.images[0]['url']
+    @popularity = album.popularity
+    @total_tracks = album.total_tracks
+    @genre = album.genres
+    total_duration_ms = album.tracks.sum { |track| track.duration_ms }
     total_seconds = total_duration_ms / 1000
     minutes = total_seconds / 60
     seconds = total_seconds % 60
