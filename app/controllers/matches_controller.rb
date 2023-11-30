f24c5ecc6bccd500c996aa90d56e5a974d595fa1
elia benedetti <eliabenedetti04@gmail.com>
2023-11-30 14:59:20 +0100

    match page step 1

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,29 +1,82 @@
 class MatchesController < ApplicationController
   require 'rspotify/oauth'
+  before_action :set_users
 
   def index
+    unless @spotify_generator.images[0].nil?
+      @generator_image = @spotify_generator.images[0]['url']
+    end
+    unless @spotify_buddy.images[0].nil?
+      @buddy_image = @spotify_buddy.images[0]['url']
+    end
+
+    generator_top_artists = @spotify_generator.top_artists(limit: 50)
+    buddy_top_artists = @spotify_buddy.top_artists(limit: 50)
+
+    shared_artist_ids = generator_top_artists.map(&:id) & buddy_top_artists.map(&:id)
+    @shared_artists = shared_artist_ids.map do |id|
+      artist = RSpotify::Artist.find(id)
+      {
+        name: artist.name,
+        id: artist.id,
+        image_url: artist.images[0]['url'] # Assuming the artist has images
+      }
+    end
+
+    generator_top_tracks = @spotify_generator.top_tracks(limit: 50)
+    buddy_top_tracks = @spotify_buddy.top_tracks(limit: 50)
+
+    shared_tracks_ids = generator_top_tracks.map(&:id) & buddy_top_tracks.map(&:id)
+    @shared_tracks = shared_tracks_ids.map do |id|
+      track = RSpotify::Track.find(id)
+      {
+        name: track.name,
+        id: track.id,
+        image_url: track.album.images[0]['url'] # Assuming the track has images
+      }
+    end
+  end
+
+  def show
+    @match = Match.find(params[:id])
+    @match_playlist = @playlist
+  end
+
+  def new
+    @match = Match.new
+    recommendations = RSpotify::Recommendations.generate(limit: 50, seed_tracks: [@shared_tracks[:id].each], seed_artists: [@shared_artists[:id].each])
+    recommended_tracks = recommendations.tracks
+    @playlist = user.create_playlist!("#{@generator.display_name} + #{@buddy.display_name}")
+    @playlist.add_tracks!(recommended_tracks)
+  end
+
+  def create
+    @match = Match.new(match_params)
+    if @match.save
+      redirect_to user_matches_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_users
     @generator = current_user
-    # Pass this whenever we need to access the user's spotify account
     @spotify_generator = RSpotify::User.new(@generator.spotify_auth)
     @buddy = User.find(params[:user_id])
     @spotify_buddy = RSpotify::User.new(@buddy.spotify_auth)
+
+    # generator_endpoint = RestClient.get(
+    #   "https://api.spotify.com/v1/users/#{@spotify_generator.id}",
+    #   headers={ 'Authorization': "Bearer #{@access_token}" }
+    # )
+    # @generator_data = JSON.parse(generator_endpoint)
+
+    # buddy_endpoint = RestClient.get(
+    #   "https://api.spotify.com/v1/users/#{@spotify_buddy.id}",
+    #   headers={ 'Authorization': "Bearer #{@access_token}" }
+    # )
+    # @buddy_data = JSON.parse(buddy_endpoint)
   end
-
-  # endpoint1 = RestClient.get(
-  #   "https://api.spotify.com/v1/artists/4iHNK0tOyZPYnBU7nGAgpQ?si=1v21fDMzRjKRLTyoFyBZFA",
-  #   headers={ 'Authorization': "Bearer #{@access_token}" }
-  # )
-  # @data1 = JSON.parse(endpoint1)
-
-  # @maria = @data1['name']
-  # @maria_photo = @data1['images'][0]['url']
-
-  # endpoint2 = RestClient.get(
-  #   "https://api.spotify.com/v1/artists/5lwmRuXgjX8xIwlnauTZIP?si=zA8LyXDLSVC65iuKFPiKmg",
-  #   headers={ 'Authorization': "Bearer #{@access_token}" }
-  # )
-  # @data2 = JSON.parse(endpoint2)
-
-  # @romeo = @data2['name']
-  # @romeo_photo = @data2['images'][0]['url']
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,60 +11,19 @@ class UsersController < ApplicationController
     # Redirects, even if not successfull jsut testingg
     redirect_to user_path(@user)
 
-
-    # Now you can access user's private data, create playlists and much more
-
-    # # Access private data
-    # @spotify_user.country #=> "US"
-    # @spotify_user.email   #=> "example@email.com"
-
-    # # Create playlist in user's Spotify account
-    # @playlist = @spotify_user.create_playlist!('my-awesome-playlist')
-
-    # # Add tracks to a playlist in user's Spotify account
-    # @tracks = RSpotify::Track.search('Know')
-    # @playlist.add_tracks!(tracks)
-    # @playlist.tracks.first.name #=> "Somebody That I Used To Know"
-
-    # # Access and modify user's music library
-    # @spotify_user.save_tracks!(tracks)
-    # @spotify_user.saved_tracks.size #=> 20
-    # @spotify_user.remove_tracks!(tracks)
-
-    # @albums = RSpotify::Album.search('launeddas')
-    # @spotify_user.save_albums!(albums)
-    # @spotify_user.saved_albums.size #=> 10
-    # @spotify_user.remove_albums!(albums)
-
-    # # Use Spotify Follow features
-    # @spotify_user.follow(playlist)
-    # # spotify_user.follows?(artists)
-    # # spotify_user.unfollow(users)
-
-    # # Get user's top played artists and tracks
-    # @spotify_user.top_artists #=> (Artist array)
-    # @spotify_user.top_tracks(time_range: 'short_term') #=> (Track array)
-
-    # Check doc for more
-    # @hash = @spotify_user.to_hash
+    @hash = spotify_user.to_hash
+    # # hash containing all user attributes, including access tokens
+    # # Use the hash to persist the data the way you prefer...
+    # # Then recover the Spotify user whenever you like
+    # @spotify_user = RSpotify::User.new(hash)
   end
-  # hash containing all user attributes, including access tokens
 
-  # Use the hash to persist the data the way you prefer...
-
-  # Then recover the Spotify user whenever you like
-  # @spotify_user = RSpotify::User.new(hash)
-  # spotify_user.create_playlist!('my_awesome_playlist') # automatically refreshes token
   def show
     @user = User.find(params[:id])
-    # Pass this whenever we need to access the user's spotify account
-    @spotify_user = RSpotify::User.new(@user.spotify_auth)
-    # @matches = Match.where(generator: @user)
-    # @buddies = Match.where(buddy: @user)
 
-    @top_artists =  @spotify_user.top_artists
-    @top_tracks =  @spotify_user.top_tracks
-    # @top_tracks = @top_tracks[0..4]
-    # @top_artist = @top_artist[0..4]
+    @spotify_user = RSpotify::User.new(@user.spotify_auth)
+
+    @top_artists = @spotify_user.top_artists
+    @top_tracks = @spotify_user.top_tracks
   end
 end
