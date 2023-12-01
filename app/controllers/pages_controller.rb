5a5372baccc21e0b6b98a8804965252b95331fd5
Tiphaine Ollivier <tiphaine.ollivier1@gmail.com>
2023-12-01 11:40:28 +0100

    got top trending songs on home page + fixed tracks show a bit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,9 @@ class PagesController < ApplicationController
   before_action :get_key
 
   def home
+    user_top_tracks
+    top_fifty = RSpotify::Playlist.find("spotifycharts", "37i9dQZEVXbMDoHDwVN2tF")
+    @top_fifty_tracks = top_fifty.tracks
     # endpoint1 = RestClient.get(
     #   "https://api.spotify.com/v1/artists/3ifxHfYz2pqHku0bwx8H5J?si=rRd3U9grQJG3Vgdjn8k0oA",
     #   headers={ 'Authorization': "Bearer #{@access_token}" }
@@ -10,8 +13,13 @@ class PagesController < ApplicationController
     # @data1 = JSON.parse(endpoint1)
   end
 
-  private
+  def user_top_tracks
+    @user= current_user
+    @user_spot = RSpotify::User.new(@user.spotify_auth)
+    @user_top_tracks = @user_spot.top_tracks(limit: 20)
+  end
 
+  private
   def get_key
     response = RestClient.post 'https://accounts.spotify.com/api/token',
     { grant_type: 'client_credentials',
