5ea90629c1167cda44dd6208456ca77cf230a23d
Federico Palou <federicopalou@gmail.com>
2023-12-05 11:25:30 +0100

    Added: Sorting for follows, Style to buttons when pressed. Bunch of formatting and updates on controllers

--- app/javascript/controllers/switch_button_controller.js
+++ app/javascript/controllers/switch_button_controller.js
@@ -2,37 +2,34 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="switch-button"
 export default class extends Controller {
-  static targets = ["info", "stats", "followers", "following", "albums", "artists", "songs", "all", "playlists"]
+  static targets = ["info", "stats", "albums", "artists", "songs", "all", "playlists"]
   connect() {
-    console.log("Switch Button Controller connected!");
+    // console.log("Switch Button Controller connected!");
   }
 
-  switchToInfo() {
-    console.log("Switching to info");
+  switchToInfo(event) {
+    // console.log("Switching to info");
+    this.removeActiveClassLong();
+    let button = event.currentTarget.querySelector('.btn-flat-long');
+    button.classList.add('button-active');
     this.infoTarget.style.display = "block";
     this.statsTarget.style.display = "none";
   }
 
-  switchToStats() {
-    console.log("Switching to stats");
+  switchToStats(event) {
+    // console.log("Switching to stats");
+    this.removeActiveClassLong();
+    let button = event.currentTarget.querySelector('.btn-flat-long');
+    button.classList.add('button-active');
     this.infoTarget.style.display = "none";
     this.statsTarget.style.display = "block";
   }
 
-  switchToFollowers() {
-    console.log("Switching to info");
-    this.followersTarget.style.display = "block";
-    this.followingTarget.style.display = "none";
-  }
 
-  switchToFollowing() {
-    console.log("Switching to stats");
-    this.followersTarget.style.display = "none";
-    this.followingTarget.style.display = "block";
-  }
-
-  switchToAlbums() {
-    console.log("Switching to Albums");
+  switchToAlbums(event) {
+    // console.log("Switching to Albums");
+    this.removeActiveClassShort();
+    event.currentTarget.classList.add('button-active');
     this.albumsTarget.style.display = "block";
     this.artistsTarget.style.display = "none";
     this.songsTarget.style.display = "none";
@@ -42,8 +39,10 @@ export default class extends Controller {
     }
   }
 
-  switchToArtists() {
-    console.log("Switching to Artist");
+  switchToArtists(event) {
+    // console.log("Switching to Artist");
+    this.removeActiveClassShort();
+    event.currentTarget.classList.add('button-active');
     this.albumsTarget.style.display = "none";
     this.songsTarget.style.display = "none";
     this.allTarget.style.display = "none";
@@ -53,8 +52,10 @@ export default class extends Controller {
     this.artistsTarget.style.display = "block";
   }
 
-  switchToSongs() {
-    console.log("Switching to Songs");
+  switchToSongs(event) {
+    // console.log("Switching to Songs");
+    this.removeActiveClassShort();
+    event.currentTarget.classList.add('button-active');
     this.albumsTarget.style.display = "none";
     this.artistsTarget.style.display = "none";
     this.allTarget.style.display = "none";
@@ -64,8 +65,10 @@ export default class extends Controller {
     this.songsTarget.style.display = "block";
   }
 
-  switchToPlaylists() {
-    console.log("Switching to Playlists");
+  switchToPlaylists(event) {
+    // console.log("Switching to Playlists");
+    this.removeActiveClassShort();
+    event.currentTarget.classList.add('button-active');
     this.albumsTarget.style.display = "none";
     this.artistsTarget.style.display = "none";
     this.songsTarget.style.display = "none";
@@ -75,8 +78,10 @@ export default class extends Controller {
     }
   }
 
-  switchToAll() {
-    console.log("Switching to All");
+  switchToAll(event) {
+    // console.log("Switching to All");
+    this.removeActiveClassShort();
+    event.currentTarget.classList.add('button-active');
     this.albumsTarget.style.display = "none";
     this.artistsTarget.style.display = "none";
     this.songsTarget.style.display = "none";
@@ -85,8 +90,10 @@ export default class extends Controller {
     }
     this.allTarget.style.display = "block";
   }
-  switchToAllOfIt() {
-    console.log("Switching to All");
+  switchToAllOfIt(event) {
+    // console.log("Switching to All");
+    this.removeActiveClassShort();
+    event.currentTarget.classList.add('button-active');
     this.albumsTarget.style.display = "block";
     this.artistsTarget.style.display = "block";
     this.songsTarget.style.display = "block";
@@ -94,5 +101,13 @@ export default class extends Controller {
       this.playlistsTarget.style.display = "block";
     }
     this.allTarget.style.display = "block";
+  }
+  removeActiveClassLong() {
+    const buttons = document.querySelectorAll('.btn-flat-long');
+    buttons.forEach(button => button.classList.remove('button-active'));
+  }
+  removeActiveClassShort() {
+    const buttons = document.querySelectorAll('.show-filter button');
+    buttons.forEach(button => button.classList.remove('button-active'));
   }
 }
