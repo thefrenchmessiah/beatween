626de87cf5b28f01cbb115785364ce8d2621fa72
Tiphaine Ollivier <tiphaine.ollivier1@gmail.com>
2023-12-05 23:09:36 +0100

    added the bubbles for the convo

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,7 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     @message.chatroom = @chatroom
     @message.user = current_user
+    @user = current_user
     if @message.save
       ChatroomChannel.broadcast_to(@chatroom,
         render_to_string(partial: "message", locals: {message: @message})
